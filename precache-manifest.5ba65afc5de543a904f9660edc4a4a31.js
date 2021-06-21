2451e6c8c14904062e3444c58c4c3e1e449c64ea
unknown <jatin.gehlot18@gmail.com>
2021-06-21 19:51:31 +0530

    Updates

---
+++ precache-manifest.5ba65afc5de543a904f9660edc4a4a31.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0b45f38db4a2d1ca24f8573e72085b5",
+    "url": "/JatinGehlot18/ReactNotes/index.html"
+  },
+  {
+    "revision": "3ce7f437ab3b4744e13e",
+    "url": "/JatinGehlot18/ReactNotes/static/js/2.54be8ece.chunk.js"
+  },
+  {
+    "revision": "e4e203c107a271330219",
+    "url": "/JatinGehlot18/ReactNotes/static/js/main.e1c506e8.chunk.js"
+  },
+  {
+    "revision": "7e27176cbcffc904bfd0",
+    "url": "/JatinGehlot18/ReactNotes/static/js/runtime-main.9b4ca883.js"
+  }
+]);
